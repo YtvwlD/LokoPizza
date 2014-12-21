b406d4d7876dc25e8b31ad322ac0c9cf78fced16
Niklas Sombert <niklas@ytvwld.de>
2014-12-21 17:11:12 +0100

    mehr Musikspuren gleichzeitig
    Und zwar bis zu zwei. Das sind <= 100% mehr!

--- music.py
+++ music.py
@@ -1,33 +1,38 @@
 from threading import Thread
 from wave import open as waveOpen
+from audioop import add
 from subprocess import Popen, PIPE
 
-
 class Music(Thread):
-
-    def __init__(self, lokopizza):
-        Thread.__init__(self)
-        self.lokopizza = lokopizza
-        self.scheduled = []
-        self.pa = Popen(["pacat", "--latency-msec=1", "--volume=32000", "--client=LokoPizza"], stdin=PIPE, stdout=None, stderr=None)
-
-    def run(self):
-        wave = waveOpen("bgm_mouthmoney.wav", "rb")
-        self.keeprunning = True
-        while(self.keeprunning):
-			if not self.scheduled:
-				read = wave.readframes(256)
-				if not read:
-					wave.rewind()
-					continue
-			else:
+	def __init__(self, lokopizza):
+		Thread.__init__(self)
+		self.lokopizza = lokopizza
+		self.scheduled = []
+		self.pa = Popen(["pacat", "--latency-msec=1", "--volume=32000", "--client=LokoPizza"], stdin=PIPE, stdout=None, stderr=None)
+	
+	def run(self):
+		wave = waveOpen("bgm_mouthmoney.wav", "rb")
+		self.keeprunning = True
+		while(self.keeprunning):
+			read0 = wave.readframes(4)
+			if not read0:
+				wave.rewind()
+				read0 = wave.readframes(4)
+			if self.scheduled:
 				scheduled = self.scheduled[0]
-				read = scheduled.readframes(256)
-				if not read:
+				read1 = scheduled.readframes(4)
+				if not read1:
 					self.scheduled.remove(scheduled)
-					continue
-			self.pa.stdin.write(read)
-			self.pa.stdin.flush()
+					read1 = None
+			else:
+				read1 = None
+			try:
+				res = add(read0, read1, 4)
+			except:
+				res = read0
+			if res:
+				self.pa.stdin.write(res)
+				#self.pa.stdin.flush()
 	
 	def play(self, filename):
 		wave = waveOpen(filename, "rb")
