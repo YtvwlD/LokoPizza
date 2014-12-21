213531969196f8870d93217c0b59291ecbe80336
Niklas Sombert <niklas@ytvwld.de>
2014-12-21 15:47:02 +0100

    Musik abspielen!
    Und nicht nur Hintergrundmusik.

--- music.py
+++ music.py
@@ -8,15 +8,27 @@ class Music(Thread):
     def __init__(self, lokopizza):
         Thread.__init__(self)
         self.lokopizza = lokopizza
+        self.scheduled = []
         self.pa = Popen(["pacat", "--latency-msec=1", "--volume=32000", "--client=LokoPizza"], stdin=PIPE, stdout=None, stderr=None)
 
     def run(self):
         wave = waveOpen("bgm_mouthmoney.wav", "rb")
         self.keeprunning = True
         while(self.keeprunning):
-            read = wave.readframes(256)
-            if read:
-                self.pa.stdin.write(read)
-                self.pa.stdin.flush()
-            else:
-                wave.rewind()
+			if not self.scheduled:
+				read = wave.readframes(256)
+				if not read:
+					wave.rewind()
+					continue
+			else:
+				scheduled = self.scheduled[0]
+				read = scheduled.readframes(256)
+				if not read:
+					self.scheduled.remove(scheduled)
+					continue
+			self.pa.stdin.write(read)
+			self.pa.stdin.flush()
+	
+	def play(self, filename):
+		wave = waveOpen(filename, "rb")
+		self.scheduled.append(wave)
