3a3160eec41d814a75a4d773a5a69f37f03b8451
Olaf Gladis <olaf@gladis.org>
2015-01-22 21:10:18 +0100

    better tracebacks

--- main.py
+++ main.py
@@ -6,42 +6,46 @@ from music import Music
 
 
 class LokoPizza:
-	def __init__(self):
-		if len(sys.argv) != 2:
-			print ("Please select the music to play via sys.arvg[1].")
-			print ("None, bgm, train or NootNoot.")
-			sys.exit(1)
-		self.music = Music(sys.argv[1])
-		self.screen = curses.initscr()
-		self.screen.clear()
-		self.screen.refresh()
-		self.music.start()
-		self.mapstr = "map{}.txt"
-		curses.noecho()
-		self.game = Game(self, 0)
-		self.game.start()
-	
-	def nextLevel(self):
-		level = self.game.level
-		if level:
-			level += 1
-		self.loadLevel(level)
-	
-	def loadLevel(self, level):
-		self.screen.clear()
-		self.music.reset()
-		self.game.stop()
-		try:
-			self.game = Game(self, level)
-			self.game.start()
-		except IOError:
-			self.mapstr = "map{}.txt"
-			self.loadLevel(0)
-	
-	def quit(self):
-		self.music.keeprunning = False
-		curses.endwin()
-		sys.exit(0)
+    def __init__(self):
+        if len(sys.argv) != 2:
+            print ("Please select the music to play via sys.arvg[1].")
+            print ("None, bgm, train or NootNoot.")
+            sys.exit(1)
+        self.music = Music(sys.argv[1])
+        self.screen = curses.initscr()
+        self.screen.clear()
+        self.screen.refresh()
+        self.music.start()
+        self.mapstr = "map{}.txt"
+        curses.noecho()
+        self.game = Game(self, 0)
+        self.game.start()
+    
+    def nextLevel(self):
+        level = self.game.level
+        if level:
+            level += 1
+        self.loadLevel(level)
+    
+    def loadLevel(self, level):
+        self.screen.clear()
+        self.music.reset()
+        self.game.stop()
+        try:
+            self.game = Game(self, level)
+            self.game.start()
+        except IOError:
+            self.mapstr = "map{}.txt"
+            self.loadLevel(0)
+    
+    def quit(self):
+        self.music.keeprunning = False
+        curses.endwin()
+        sys.exit(0)
 
 if __name__ == "__main__":
-	lokopizza = LokoPizza()
+    try:
+        lokopizza = LokoPizza()
+    except Exception:
+        curses.endwin()
+        raise
